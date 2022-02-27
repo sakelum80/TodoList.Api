1c88f0e328c655f67fa96c432f56f86b4b3cffed
sakthi <sakelum80@gmail.com>
2022-02-27 17:19:31 +0000

    Created Repo and Interface with abstract methods for CURD operation of TodoList

---
+++ TodoList/Repository/ITodoListRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Data.Entities;
+
+namespace TodoList.Repository
+{
+    public interface ITodoListRepo
+    {
+        IEnumerable<TodoItem> GetAllItems();
+        TodoItem GetItem(int id);
+        void CreateItem(TodoItem item);
+        void UpdateItem(TodoItem item);
+
+        void DeleteItem(TodoItem item);
+
+        bool SaveChanges();
+
+    }
+}
